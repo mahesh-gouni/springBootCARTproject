cef3058ba95d4dc66f5f67e892a7a9f64182724e
gouni <gounimahesh9000@gmail.com>
2024-09-19 16:54:23 +0530

    check

---
+++ src/test/java/com/springboot/cartProject/CartProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.cartProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
